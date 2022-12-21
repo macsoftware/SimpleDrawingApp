0791a75697a39e25bc2b34bd54f6ae5cb47dcf8a
macsoftware <87543117+macsoftware@users.noreply.github.com>
2022-12-21 23:42:10 +0000

    Added ability to draw, undo, redo and have a background image. You can also save and share the image. Needs tidying up.

--- app/src/main/java/uk/co/macsoftware/simpledrawingapp/DrawingView.kt
+++ app/src/main/java/uk/co/macsoftware/simpledrawingapp/DrawingView.kt
@@ -1,4 +1,134 @@
 package uk.co.macsoftware.simpledrawingapp
 
-class DrawingView {
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private var mDrawPath: CustomPath? = null
+
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint?= null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var colour = Color.BLACK
+    private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
+    private val mUndoPaths = ArrayList<CustomPath>()
+
+    init{
+        setupDrawing()
+    }
+
+    fun onClickUndo(){
+        if(mPaths.size > 0){
+            mUndoPaths.add(mPaths.removeAt(mPaths.size - 1))
+            //this will clear the view and recall the onDraw method.
+            invalidate()
+        }
+    }
+
+    fun onClickRedo(){
+        if(mUndoPaths.size > 0){
+            mPaths.add(mUndoPaths.removeAt(mUndoPaths.size - 1))
+            //this will clear the view and recall the onDraw method.
+            invalidate()
+        }
+    }
+
+    private fun setupDrawing(){
+
+        mDrawPath = CustomPath(colour, mBrushSize)
+
+        mDrawPaint = Paint()
+        mDrawPaint!!.color = colour
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+
+        //mBrushSize = 20.toFloat()
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for (path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.colour
+            canvas.drawPath(path, mDrawPaint!!)
+
+        }
+
+        if(!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.colour
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action){
+            MotionEvent.ACTION_DOWN ->{
+                mDrawPath!!.colour = colour
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                if(touchX != null && touchY != null)
+                    mDrawPath!!.moveTo(touchX, touchY)
+            }
+            MotionEvent.ACTION_MOVE ->{
+                if(touchX != null && touchY != null)
+                    mDrawPath!!.lineTo(touchX, touchY)
+            }
+            MotionEvent.ACTION_UP ->{
+                mPaths.add(mDrawPath!!)
+                mDrawPath = CustomPath(colour, mBrushSize)
+            }
+            else -> return false
+
+        }
+        invalidate()
+
+        return true
+    }
+
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    fun setColour(newColour: String){
+        colour = Color.parseColor(newColour)
+        mDrawPaint!!.color = colour
+    }
+
+    internal inner class CustomPath(var colour: Int, var brushThickness: Float) : Path() {
+
+    }
+
+
 }
